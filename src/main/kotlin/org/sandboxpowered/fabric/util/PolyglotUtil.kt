d0a526b6409924c9f368dda40e2e2f2e30dad0ec
Coded <coded@voxel.games>
2021-08-15 14:57:51 +0100

    Cleanup value json conversion method

--- src/main/kotlin/org/sandboxpowered/fabric/util/PolyglotUtil.kt
+++ src/main/kotlin/org/sandboxpowered/fabric/util/PolyglotUtil.kt
@@ -7,32 +7,24 @@ import org.graalvm.polyglot.Value
 fun Value.getMemberValue(identifier: String): String? =
     if (hasMember(identifier)) getMember(identifier).asString() else null
 
-fun Value.toJSON(): JsonElement {
-    when {
-        hasArrayElements() -> {
-            val array = JsonArray()
-            for (idx in 0 until arraySize) {
-                val element = getArrayElement(idx)
-                array.add(element.toJSON())
-            }
-            return array
+fun Value.toJSON(): JsonElement = when {
+    hasArrayElements() -> {
+        val array = JsonArray()
+        for (idx in 0 until arraySize) {
+            val element = getArrayElement(idx)
+            array.add(element.toJSON())
         }
-        hasMembers() -> {
-            val obj = JsonObject()
-            for (memberKey in memberKeys) {
-                obj.add(memberKey, getMember(memberKey).toJSON())
-            }
-            return obj
-        }
-        isString -> {
-            return JsonPrimitive(asString())
-        }
-        isBoolean -> {
-            return JsonPrimitive(asBoolean())
-        }
-        isNumber -> {
-            return JsonPrimitive(asInt())
-        }
-        else -> return JsonNull.INSTANCE
+        array
     }
+    hasMembers() -> {
+        val obj = JsonObject()
+        for (memberKey in memberKeys) {
+            obj.add(memberKey, getMember(memberKey).toJSON())
+        }
+        obj
+    }
+    isString -> JsonPrimitive(asString())
+    isBoolean -> JsonPrimitive(asBoolean())
+    isNumber -> JsonPrimitive(asInt())
+    else -> JsonNull.INSTANCE
 }
