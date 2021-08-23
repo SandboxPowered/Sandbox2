8b6139e00ff8bc6975e52681433045912e1d729d
Coded <coded@voxel.games>
2021-08-23 13:52:46 +0100

    Cleanup polyglot util

--- src/main/kotlin/org/sandboxpowered/fabric/util/PolyglotUtil.kt
+++ src/main/kotlin/org/sandboxpowered/fabric/util/PolyglotUtil.kt
@@ -3,18 +3,13 @@ package org.sandboxpowered.fabric.util
 import com.google.gson.*
 import org.graalvm.polyglot.Value
 
+fun Value.getMemberValue(member: String): Value? = if (hasMember(member)) getMember(member) else null
 
-fun Value.getMemberValueStr(identifier: String): String? =
-    if (hasMember(identifier)) getMember(identifier).asString() else null
+fun Value.getMemberValueStr(member: String): String? = getMemberValue(member)?.asString()
+fun Value.getMemberValueInt(member: String): Int? = getMemberValue(member)?.asInt()
 
-fun Value.getMemberValue(identifier: String, default: String): String =
-    if (hasMember(identifier)) getMember(identifier).asString() else default
-
-fun Value.getMemberValueInt(identifier: String): Int? =
-    if (hasMember(identifier)) getMember(identifier).asInt() else null
-
-fun Value.getMemberValue(identifier: String, default: Int): Int =
-    if (hasMember(identifier)) getMember(identifier).asInt() else default
+fun Value.getMemberValue(member: String, default: String): String = getMemberValueStr(member) ?: default
+fun Value.getMemberValue(member: String, default: Int): Int = getMemberValueInt(member) ?: default
 
 fun Value.toJSON(): JsonElement = when {
     hasArrayElements() -> JsonArray().apply {
